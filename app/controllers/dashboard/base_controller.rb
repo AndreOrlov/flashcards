053e9237b410052ee1187cfef3d29591615e7b54
4xtrot <hipilotage@gmail.com>
2016-08-10 16:44:51 +0300

    refactoring method index. Put it to base_controller.rb

--- app/controllers/dashboard/base_controller.rb
+++ app/controllers/dashboard/base_controller.rb
@@ -2,6 +2,25 @@ class Dashboard::BaseController < ApplicationController
   before_action :require_login
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
+  def index
+    if params[:id]
+      @card = current_user.cards.find(params[:id])
+    else
+      if current_user.current_block
+        @card = current_user.current_block.cards.pending.first
+        @card ||= current_user.current_block.cards.repeating.first
+      else
+        @card = current_user.cards.pending.first
+        @card ||= current_user.cards.repeating.first
+      end
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
   private
 
   def not_authenticated
