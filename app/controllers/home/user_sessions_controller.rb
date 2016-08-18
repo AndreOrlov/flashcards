74eaf227e24d1b9eab8e8f06de42da17b173a45f
4xtrot <hipilotage@gmail.com>
2016-08-18 21:02:10 +0300

    Edited style programming

--- app/controllers/home/user_sessions_controller.rb
+++ app/controllers/home/user_sessions_controller.rb
@@ -9,11 +9,11 @@ class Home::UserSessionsController < Home::BaseController
 
   def create
     @user = login(params[:email], params[:password])
-    unless @user.nil?
-      redirect_back_or_to root_path, notice: t(:log_in_is_successful_notice)
-    else
+    if @user.nil?
       flash.now[:alert] = t(:not_logged_in_alert)
       render action: 'new'
+    else
+      redirect_back_or_to root_path, notice: t(:log_in_is_successful_notice)
     end
   end
 end
